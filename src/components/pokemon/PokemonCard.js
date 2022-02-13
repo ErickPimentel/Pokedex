be5ecc4ce573bb6275fd61768c8247baff36bb27
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-02-13 13:43:53 -0300

    loading gif added

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 import styled from 'styled-components';
 
+import spinner from '../pokemon/spinner.gif'
+
 const Sprite = styled.img`
 width: 5em;
 height: 5em;
+display: none;
 `
 
 export default class PokemonCard extends Component {
@@ -21,7 +24,7 @@ export default class PokemonCard extends Component {
     const { name, url } = this.props
     const pokemonIndex = url.split("/")[url.split('/').length - 2];
     const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
-    
+
     this.setState({
       name,
       imageUrl,
@@ -38,12 +41,25 @@ export default class PokemonCard extends Component {
           <h5 className='card-header'>
             {this.state.pokemonIndex}
           </h5>
+          {this.state.imageLoading ? (
+            <img 
+            src={spinner} 
+            style={{width: '5em', height: '5em'}}
+            className="card-img-top rounded mx-auto d-block mt-2"></img>
+          ) : null}
           <Sprite className='card-img-top rounded mx-auto mt-2'
-          onLoad={() => this.setState({imageLoading: false})}
-          onError={() => this.setState({toManyRequests: true})}
-          src={this.state.imageUrl}
-          >
-          </Sprite>
+            onLoad={() => this.setState({ imageLoading: false })}
+            onError={() => this.setState({ toManyRequests: true })}
+            src={this.state.imageUrl}
+            style={
+              this.state.toManyRequests ? {display: "none" }:
+              this.state.imageLoading ? null : {display: "block"}
+            }
+          />
+          {this.state.toManyRequests ? (
+            <h6 className="badge badge-danger mt-2">To Many Requests</h6>
+          ) : null}
+
           <div className='card-body mx-auto'>
             <h6 className='card-title'>
               {this.state.name
