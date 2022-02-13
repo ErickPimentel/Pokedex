46376055640b34ea9edf1392c0e29f78330303e2
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-02-13 13:13:45 -0300

    getting pokemon indexes

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -8,17 +8,39 @@ export default class PokemonCard extends Component {
     pokemonIndex: ''
   }
 
-  render() {
-    const {name, url} = this.props
+  componentDidMount() {
+    const { name, url } = this.props
+    const pokemonIndex = url.split("/")[url.split('/').length - 2];
+    const imageUrl = `https://github.com/PokeAPI/sprits/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
 
+    this.setState({
+      name,
+      imageUrl,
+      pokemonIndex
+    })
+
+  }
+
+  render() {
 
     return (
       <div className='col-md-3 col-sm-6 mb-5'>
-          <div className='card'>
-          <div className='card-header'>
-              <h1>{name}</h1>
+        <div className='card'>
+          <h5 className='card-header'>
+            {this.state.pokemonIndex}
+          </h5>
+          <div className='card-body mx-auto'>
+            <h6 className='card-title'>
+              {this.state.name
+                .toLowerCase()
+                .split(' ')
+                .map(
+                  letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                )
+                .join(' ')}
+            </h6>
           </div>
-          </div>
+        </div>
       </div>
     )
   }
